c5b4ba2688b9eb4aee3db7b9646b9b6dc79c6c08
Hass090 <burcevila91@gmail.com>
2025-12-26 13:48:26 +0000

    Add server: MariaDB schema (sensors, events, access_logs)

--- server/db/database_schema.sql
+++ server/db/database_schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE sensors (
-    id_sens INT AUTO_INCREMENT PRIMARY KEY,
+    id_sensors INT AUTO_INCREMENT PRIMARY KEY,
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
     temperature FLOAT,
     humidity FLOAT,
@@ -7,14 +7,14 @@ CREATE TABLE sensors (
 );
 
 CREATE TABLE events (
-    id_even INT AUTO_INCREMENT PRIMARY KEY,
+    id_events INT AUTO_INCREMENT PRIMARY KEY,
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
     type VARCHAR(50),
     description VARCHAR(255)
 );
 
 CREATE TABLE access_logs (
-    id_accs_logs INT AUTO_INCREMENT PRIMARY KEY,
+    id_access_logs INT AUTO_INCREMENT PRIMARY KEY,
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
     method VARCHAR(20),
     user_name VARCHAR(50),
